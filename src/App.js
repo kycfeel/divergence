e5c255bd528726f9a4f5c41d36f913ed0f730bfe
Yechan Kim <kycfeel@gmail.com>
2017-05-23 16:21:56 +0900

    First commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Whatamidoing from './Whatamidoing.js';
+import ReactCountdownClock from 'react-countdown-clock';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-Task">
+        <Whatamidoing task = {prompt("지금 하고있는 일이 무엇인가요?")}/>
+        </div>
+        <div className="App-Countdown">
+        <ReactCountdownClock seconds={prompt("몇초 뒤 돌아올 예정이신가요?")}
+                  color="#27a0f4"
+                  alpha={0.9}
+                  size={210}
+                  onComplete="시간 만료!"/>
+        </div>
+        <div className="App-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">divergence</h5>
+                <p class="grey-text text-lighten-4">잠시 자리를 비울 때 다른 사람들에게 내가 왜 나갔고, 언제 돌아오는지 알려줄 수 있어요.</p>
+                <p class="grey-text text-lighten-4">내가 입력하는 모든 정보는 암호화되어 올라가고, 브라우저를 닫으면 완전히 삭제됩니다. 안심하고 사용하세요.</p>
+              </div>
+          </div>
+        </div>
+        <div class="footer-copyright">
+          <div class="container">
+          <a class="white-text" href="">GitHub</a> 에서 소스코드를 확인해 보세요. Made by <a class="orange-text text-lighten-3" href="https://twitter.com/kycfeel">kycfeel</a>, Theme from <a class="orange-text text-lighten-3" href="http://materializecss.com">Materialize.</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    );
+  }
+}
+
+export default App;
---
+++ src/Whatamidoing.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Whatamidoing extends Component {
+    render() {
+        return (
+            <div>
+              <h2>나는 지금 <a class="orange-text text-lighten-3">{this.props.task}</a> 중이에요!</h2>
+              <h5>아래 시간이 끝나기 전까지 돌아올게요.</h5>
+            </div>
+        );
+    }
+}
+
+export default Whatamidoing;
